687736dfd09e811a6d8618b8cf203ed514b376d2
kasia-filip <95070763+kasia-filip@users.noreply.github.com>
2022-01-14 12:31:38 +0100

    Project setup (#3)
    * Update README.md
    
    * setting up project
    
    * adding changes in package-lock.json
    
    * changes in tsconfig.json
    
    * final setup'
    
    * update
    
    * update
    
    * added redirect and removed vscode folder
    
    * removed 'old' folder
    
    * fixing issues reported in Code Review
    
    * fixing issues form CR
    
    Co-authored-by: Aleksander Atamańczuk <56917808+TenGosc007@users.noreply.github.com>
    Co-authored-by: kfilip <kfilip@imgw.pl>

--- src/state/Puppylog/puppylogSlice.ts
+++ src/state/Puppylog/puppylogSlice.ts
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// export const slice = createSlice(
-//     name: 'puppylog',
-//     initialState: {},
-//     reducers: {}
-// )
+export const slice = createSlice({
+  name: "puppylog",
+  initialState: {},
+  reducers: {},
+});
 
-// export const { } = slice.actions;
+export default slice.reducer;
