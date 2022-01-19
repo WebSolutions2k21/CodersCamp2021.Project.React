6c8537e58d3a4c7714c2a0895fa313bd56057f0e
Marta Probierz <marta.anna.probierz@gmail.com>
2022-01-19 13:59:36 +0100

    bugfix: add separate file for style

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,22 +1,7 @@
 import { useState } from 'react';
-import { makeStyles } from '@mui/styles';
 import TextField from '@mui/material/TextField';
+import { useStyles } from './InputStyle';
 
-
-const useStyles = makeStyles({
-    int: {
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-        width: '250px',
-        borderRadius: '8px',
-    },
-    lbl: {
-        fontSize: '16px',
-        lineHeight: '18px',
-        color: '#959CA0',
-        marginLeft: '17px',
-        marginRight: '17px'
-    }
-});
 
 interface Props {
     label: string;
