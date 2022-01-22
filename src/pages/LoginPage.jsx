9dcf2f279cc0e4811fb030288a663b98a845c2ac
SolWika <solwika@github.com>
2022-01-23 00:13:34 +0100

    Add headlines, inputs, buttons for login page, add weight fonts

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,27 +9,83 @@ import { Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
+const useStyle = makeStyles({
+  h1: {
+    fontFamily: 'Ubuntu, sans-serif;',
+    fontWeight: '400',
+    color: '#16BAC6',
+  },
+});
+
 export const LoginPage = () => {
+  const classes = useStyle();
   return (
     <Layout>
       <Grid container direction="column" alignItems="center" style={{ marginTop: '20vmin' }} gap="2rem">
-        <Typography theme={LoginPageTheme} variant="h1" component="h1" color="#16BAC6" fontSize="3.8rem">
+        <Typography variant="h1" component="h1" className={classes.h1}>
           Log In
         </Typography>
         <Input label="email" />
         <Input label="password" />
         <Grid container justifyContent="center" gap="2rem">
-          <CustomButton text="I'm a Petlover" />
-          <CustomButton text="I'm a Doctor" />
+          <CustomButton />
+          <CustomButton />
         </Grid>
-        <Typography theme={LoginPageTheme}>
+        <Typography>
           You don't have account?
-          <Link underline="none" color="#16BAC6">
-            {' '}
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => {
+              console.info("I'm a button.");
+            }}
+          >
             Sign In!
           </Link>
         </Typography>
       </Grid>
+      {/* <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          height: '80vh',
+        }}
+      >
+        <Typography variant="h1" component="h1" className={classes.h1}>
+          Log In
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Box sx={{ margin: '20px' }}>
+            <Input label="email" />
+          </Box>
+          <Box sx={{ margin: '20px' }}>
+            <Input label="password" />
+          </Box>
+        </Box>
+        <Box>
+          <CustomButton />
+          <CustomButton />
+        </Box>
+        <Typography>
+          You don't have account?
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => {
+              console.info("I'm a button.");
+            }}
+          >
+            Sign In!
+          </Link>
+        </Typography>
+      </Box> */}
     </Layout>
   );
 };
