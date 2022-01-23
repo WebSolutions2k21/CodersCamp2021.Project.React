3ca9dbb7e3dcbb322d00c6dbe03cf52f59fe5b3b
SolWika <solwika@github.com>
2022-01-23 10:21:07 +0100

    Refactor: LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,8 +11,6 @@ import Link from '@mui/material/Link';
 
 const useStyle = makeStyles({
   h1: {
-    fontFamily: 'Ubuntu, sans-serif;',
-    fontWeight: '400',
     color: '#16BAC6',
   },
 });
@@ -22,70 +20,23 @@ export const LoginPage = () => {
   return (
     <Layout>
       <Grid container direction="column" alignItems="center" style={{ marginTop: '20vmin' }} gap="2rem">
-        <Typography variant="h1" component="h1" className={classes.h1}>
+        <Typography theme={LoginPageTheme} className={classes.h1} variant="h1" component="h1">
           Log In
         </Typography>
         <Input label="email" />
         <Input label="password" />
         <Grid container justifyContent="center" gap="2rem">
-          <CustomButton />
-          <CustomButton />
+          <CustomButton text="I'm a Petlover"> </CustomButton>
+          <CustomButton text="I'm a Doctor" />
         </Grid>
-        <Typography>
+        <Typography theme={LoginPageTheme}>
           You don't have account?
-          <Link
-            component="button"
-            variant="body2"
-            onClick={() => {
-              console.info("I'm a button.");
-            }}
-          >
+          <Link underline="none" color="#16BAC6">
+            {' '}
             Sign In!
           </Link>
         </Typography>
       </Grid>
-      {/* <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          height: '80vh',
-        }}
-      >
-        <Typography variant="h1" component="h1" className={classes.h1}>
-          Log In
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <Box sx={{ margin: '20px' }}>
-            <Input label="email" />
-          </Box>
-          <Box sx={{ margin: '20px' }}>
-            <Input label="password" />
-          </Box>
-        </Box>
-        <Box>
-          <CustomButton />
-          <CustomButton />
-        </Box>
-        <Typography>
-          You don't have account?
-          <Link
-            component="button"
-            variant="body2"
-            onClick={() => {
-              console.info("I'm a button.");
-            }}
-          >
-            Sign In!
-          </Link>
-        </Typography>
-      </Box> */}
     </Layout>
   );
 };
