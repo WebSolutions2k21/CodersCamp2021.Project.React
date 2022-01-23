49dabb1bdbd1179a2f99a8dbb278b3aa0d365cf5
kfilip <katarzyna.filip87@gmail.com>
2022-01-23 11:43:21 +0100

    build: adding button sizes

--- src/components/Button/CustomButton.jsx
+++ src/components/Button/CustomButton.jsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
 import { ButtonTheme } from '../../styles/themes/CustomButtonTheme';
-
 export const CustomButton = ({ text = 'ADD PET', color, size, clickAction }) => {
   let handleClick = () => {
     clickAction();
@@ -23,7 +22,7 @@ export const CustomButton = ({ text = 'ADD PET', color, size, clickAction }) => 
 
 // <CustomButton
 //        color="primary/secondary" - pick one of the themes
-//        size="small/medium/large" - pick one of the sizes
+//        size="small/large" - pick one of the sizes
 //        clickAction={() => {
 //          insert your function eg. alert('Klikam się!');
 //        }}
