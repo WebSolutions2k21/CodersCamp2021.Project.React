f238bf3d246ed5d8bcb85dcba9d0942ace09d096
SolWika <solwika@github.com>
2022-01-23 10:51:59 +0100

    Refactor LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,29 +6,20 @@ import { CustomButton } from '../components/Button/CustomButton';
 import { LoginPageTheme } from '../styles/themes/CostomLogInPage';
 
 import { Grid } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
-const useStyle = makeStyles({
-  h1: {
-    color: '#16BAC6',
-  },
-});
-
 export const LoginPage = () => {
-  const classes = useStyle();
   return (
     <Layout>
       <Grid container direction="column" alignItems="center" style={{ marginTop: '20vmin' }} gap="2rem">
-        <Typography theme={LoginPageTheme} className={classes.h1} variant="h1" component="h1">
+        <Typography theme={LoginPageTheme} variant="h1" component="h1" color="#16BAC6" fontSize="60px">
           Log In
         </Typography>
         <Input label="email" />
         <Input label="password" />
         <Grid container justifyContent="center" gap="2rem">
-          <CustomButton text="I'm a Petlover"> </CustomButton>
+          <CustomButton text="I'm a Petlover" />
           <CustomButton text="I'm a Doctor" />
         </Grid>
         <Typography theme={LoginPageTheme}>
--- src/styles/themes/CostomLogInPage.jsx
+++ src/styles/themes/CostomLogInPage.jsx
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import '../fonts.scss';
 
 export const LoginPageTheme = createTheme({
