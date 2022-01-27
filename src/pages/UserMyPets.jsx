2dbae17290765a461c540c70645134627dc1018a
margiebed <margiebed@gmail.com>
2022-01-27 20:28:24 +0100

    My pets page (#36)
    * feature: add pet cd
    
    * feature: add pet cd
    
    * feature: add pet cd
    
    * refaxtor: fix display pets
    
    Co-authored-by: Malgorzata Bednarczuk <margie@ais.pl>

--- src/pages/UserMyPets.jsx
+++ src/pages/UserMyPets.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
-import { Layout } from '../components';
-import { Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { CustomButton, Layout, MyPetCard } from '../components';
+import { Typography, Grid, Link } from '@mui/material';
+
+import { db } from '../config/firebase';
+import { paths } from '../config/paths';
+import { Box } from '@mui/system';
 
 export const UserMyPets = () => {
+  const [loading, setLoading] = useState(true);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const getPetsFromFirebase = [];
+    const subscriber = db.collection('pets').onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        getPetsFromFirebase.push({
+          ...doc.data(),
+          key: doc.id,
+        });
+      });
+      setPets(getPetsFromFirebase);
+      setLoading(false);
+    });
+
+    return () => subscriber();
+  }, [loading]);
+
+  if (loading) {
+    return <h1>loading data...</h1>;
+  }
+
   return (
     <Layout showSideBar>
-      <Typography paragraph>My Pets</Typography>
+      <Typography paragraph marginLeft="20px" marginTop="20px" variant="h4" color="#16bac6">
+        My Pets
+      </Typography>
+      <Box width='70vw'>
+      <Grid container spacing={4} paddingLeft="40px" paddingRight="40px" gridAutoColumns="2" margin="0">
+        {pets.length > 0 ? (
+          pets.map((pet) => (
+            <Grid item xs={12} sm={12} md={6} key={pet.key}>
+              <MyPetCard name={pet.name} breed={pet.breed} type={pet.species} age={pet.petAge} />
+            </Grid>
+          ))
+        ) : (
+          <h1>no pets yet :(</h1>
+        )}
+        <Grid container direction="column" alignItems="center" gap="1rem" item>
+          <Link component={RouterLink} to={paths.addPet} style={{ textDecoration: 'none', justifyContent: 'center' }}>
+            <CustomButton text="Add pet" />
+          </Link>
+        </Grid>
+      </Grid></Box>
     </Layout>
   );
 };
