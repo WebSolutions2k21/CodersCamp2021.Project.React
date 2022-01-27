3bc6016f4f209472512c234513c999beee9d7c8c
Marta Probierz <marta.anna.probierz@gmail.com>
2022-01-27 18:25:05 +0100

    feature: create InputFullDate and InputTime

---
+++ src/components/Inputs/InputFullDate.jsx
@@ -0,0 +1,50 @@
+// All comments are for testing what this input looks like
+
+// import { useState } from 'react';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { LocalizationProvider, DatePicker } from '@mui/lab/';
+import { TextField } from '@mui/material/';
+
+import { useStyles } from './InputStyle';
+
+
+export const InputFullDate = ({ label, value, setValue }) => {
+    const classes = useStyles();
+    // const [date, setDate] = useState(new Date());
+    
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+                views={['day', 'month', 'year']}
+                inputFormat="d MMMM y"
+                label={<div className={classes.lbl2}>{label}</div>}
+                minDate={Date.now()}
+                value={value}
+                onChange={handleChange}
+                // value={date}
+                // onChange={(newDate) => {
+                //   setDate(newDate);
+                // }}
+                renderInput={(params) => <TextField 
+                    className={classes.int} 
+                    variant="standard"
+                    {...params}
+                    sx={{
+                        svg: {
+                            color:"#16BAC6", 
+                            mb:"7px", 
+                            mr:"3px"},
+                        
+                      }}/>}
+                    InputProps={{
+                        className: classes.lbl,
+                        disableUnderline: true,
+                    }} 
+            /> 
+        </LocalizationProvider>
+  );
+}
---
+++ src/components/Inputs/InputTime.jsx
@@ -0,0 +1,46 @@
+// All comments are for testing what this input looks like
+
+// import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { LocalizationProvider, DesktopTimePicker } from '@mui/lab/';
+
+import { useStyles } from './InputStyle';
+
+
+export const InputTime = ({ label, value, setValue }) => {
+    const classes = useStyles();
+    //const [value, setValue] = useState(new Date('2018-01-01T00:00:00.000Z'));
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DesktopTimePicker
+            label={<div className={classes.lbl2}>{label}</div>}
+            value={value}
+            onChange={handleChange}
+            // value={value}
+            // onChange={(newValue) => {
+            //     setValue(newValue);
+            // }}
+            renderInput={(params) => <TextField 
+                className={classes.int} 
+                variant="standard" 
+                {...params} 
+                sx={{
+                    svg: {
+                        color:"#16BAC6", 
+                        mb:"9px", 
+                        mr:"3px"}
+                  }}/>}
+            InputProps={{
+                className: classes.lbl,
+                disableUnderline: true,
+                }}
+            />
+        </LocalizationProvider>
+    );
+}
