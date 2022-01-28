0571d09eadfee57ed56b77480fa264a7ad363cbf
SolWika <solwika@github.com>
2022-01-28 14:59:11 +0100

    Build: React Toastify

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -3,38 +3,37 @@ import TextField from '@mui/material/TextField';
 
 import { useStyles } from './InputStyle';
 
-
 export const Input = ({ label, type='text', value, setValue, fullWidth }) => {
-    const classes = useStyles();
-    const [dataError, setDataError] = useState(false);
+  const classes = useStyles();
+  const [dataError, setDataError] = useState(false);
 
-    const handleSubmit = (e) => {
-          e.preventDefault();
-          setDataError(!value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDataError(!value);
       } 
 
     const handleChange = ({ target: { value }}) => setValue && setValue(value);
 
-    return(
-          <TextField  
+  return (
+    <TextField
               fullWidth
-              onSubmit={handleSubmit}
-              onChange={handleChange}
-              value={value}
-              className={classes.int}                
-              variant="standard"
-              InputProps={{
-                  className: classes.lbl,
-                  disableUnderline: true
-                }}
-              InputLabelProps={{
-                  className: classes.lbl
-              }}                
-              label={label}
-              error={dataError}
+      onSubmit={handleSubmit}
+      onChange={handleChange}
+      value={value}
+      className={classes.int}
+      variant="standard"
+      InputProps={{
+        className: classes.lbl,
+        disableUnderline: true,
+      }}
+      InputLabelProps={{
+        className: classes.lbl,
+      }}
+      label={label}
+      error={dataError}
               helperText="Incorrect entry"
-              type={type}
+      type={type}
               sx={{ width: { xs: '250px', md: `${fullWidth&&"100%"}` }}}
-          />  
-        )
-}
+    />
+  );
+};
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,10 +11,27 @@ import { LoginPageTheme } from '../styles/themes/CustomLogInPage';
 import { Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import PropTypes from 'prop-types';
 import { Link as RouterLink, MemoryRouter } from 'react-router-dom';
 import { StaticRouter } from 'react-router-dom/server';
+
+// const auth = getAuth();
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    console.log('logged: ', user);
+    // ...
+  } else {
+    console.log('NOT logged: ', user);
+    // User is signed out
+    // ...
+  }
+});
 
 function Router(props) {
   const { children } = props;
@@ -30,11 +47,26 @@ Router.propTypes = {
 };
 
 export const LoginPage = () => {
+  const loginSucces = () => {
+    toast.success('Successful Login!', {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: '4000',
+    });
+  };
+  const loginError = () => {
+    toast.error('Login failed! Check login or password!', {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: '4000',
+    });
+  };
+
   const signIn = async () => {
     try {
       await auth.signInWithEmailAndPassword(email, password);
       console.log('po sign in');
+      loginSucces();
     } catch (error) {
+      loginError();
       console.error(error);
     }
   };
@@ -54,6 +86,7 @@ export const LoginPage = () => {
           <CustomButton text="I'm a Petlover" size="large" clickAction={() => signIn()} />
           <CustomButton text="I'm a Doctor" size="large" />
         </Grid>
+        <ToastContainer />
         <Typography theme={LoginPageTheme}>
           You don't have account?
           <Link component={RouterLink} to="/Signup" underline="none" color="#16BAC6">
