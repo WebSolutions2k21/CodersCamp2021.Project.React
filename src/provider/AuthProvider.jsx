816529ea4355a37eb61f603af53d6192f5d6199f
Anna Koruc <aniakoruc@gmail.com>
2022-02-06 00:47:16 +0100

    fix logout after refresh

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -4,15 +4,21 @@ import { AuthContext } from '../context/AuthContext';
 import { auth } from '../config/firebase';
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const [pending, setPending] = useState(true);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((firebaseUser) => {
       setUser(firebaseUser);
+      setPending(false);
     });
 
     return unsubscribe;
   }, []);
+
+  if (pending) {
+    return <>Loading ...</>;
+  }
 
   return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
