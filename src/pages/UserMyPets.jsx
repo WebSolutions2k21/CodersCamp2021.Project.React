c0dcf913b26fbf22e92536863fa5827a5ffca6a8
margiebed <margiebed@gmail.com>
2022-02-06 10:42:46 +0100

    Fix changes navigation -add MyAccount, and move delete to card (#60)

--- src/pages/UserMyPets.jsx
+++ src/pages/UserMyPets.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
 import { CustomButton, Layout, MyPetCard } from '../components';
-import { Typography, Grid, Link, Box } from '@mui/material';
+import { Typography, Grid, Link } from '@mui/material';
 
 import { db, auth } from '../config/firebase';
 import { paths } from '../config/paths';
@@ -31,61 +31,50 @@ export const UserMyPets = () => {
           }),
         );
       });
-    return ()=>setLoading(false);
+    return () => setLoading(false);
   }, [loading, user.uid]);
-
-  const deletePet = (id) => {
-    db.collection('pets').doc(id).delete();
-  };
 
   return (
     <Layout showSideBar>
-      <Typography paragraph marginLeft="20px" marginTop="20px" variant="h4" color="#16bac6">
+      <Typography paragraph marginLeft="4%" marginTop="20px" variant="h4" color="#16bac6">
         My Pets
       </Typography>
-      <Box>
-        <Grid
-          container
-          spacing={4}
-          paddingLeft="40px"
-          paddingRight="40px"
-          paddingBottom={10}
-          gridAutoColumns="2"
-          margin="0"
-        >
-          {pets.length > 0 ? (
-            pets.map((pet) => {
-              return (
-                <Grid item xs={12} sm={12} md={6} key={pet.key}>
-                  <MyPetCard
-                    name={pet.name}
-                    breed={pet.breed}
-                    type={pet.type}
-                    age={new Date(pet.petAge.seconds * 1000 + pet.petAge.nanoseconds / 1000000).toLocaleDateString(
-                      undefined,
-                      options,
-                    )}
-                  />
-                  <CustomButton
-                    edge="end"
-                    aria-label="delete"
-                    color="secondary"
-                    clickAction={() => deletePet(pet.key)}
-                    text="Delete"
-                  />
-                </Grid>
-              );
-            })
-          ) : (
-            <h1>no pets yet :(</h1>
-          )}
-          <Grid container direction="column" alignItems="center" gap="1rem" item>
-            <Link component={RouterLink} to={paths.addPet} style={{ textDecoration: 'none', justifyContent: 'center' }}>
-              <CustomButton text="Add pet" size="small" />
-            </Link>
-          </Grid>
+
+      <Grid
+        container
+        justifyContent={'center'}
+        spacing={4}
+        paddingBottom={10}
+        gridAutoColumns="2"
+        margin="auto"
+        width={'90%'}
+      >
+        {pets.length > 0 ? (
+          pets.map((pet) => {
+            return (
+              <Grid item xs={12} sm={12} md={6} key={pet.key}>
+                <MyPetCard
+                  name={pet.name}
+                  breed={pet.breed}
+                  type={pet.type}
+                  age={new Date(pet.petAge.seconds * 1000 + pet.petAge.nanoseconds / 1000000).toLocaleDateString(
+                    undefined,
+                    options,
+                  )}
+                  deleteAction={pet.key}
+                />
+              </Grid>
+            );
+          })
+        ) : (
+          <h1>no pets yet :(</h1>
+        )}
+        <Grid container direction="column" alignItems="center" gap="1rem" item  zIndex="1">
+          <Link component={RouterLink} to={paths.addPet} style={{ textDecoration: 'none', justifyContent: 'center' }}>
+            <CustomButton text="Add pet" size="large" />
+          </Link>
         </Grid>
-      </Box>
+      </Grid>
     </Layout>
   );
 };
