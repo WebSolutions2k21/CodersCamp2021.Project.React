35d7476f5560f7faef502d8d6b8ca02872428384
Malgorzata Bednarczuk <margie@ais.pl>
2022-02-07 12:57:05 +0100

    refactor: changes as develop

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -9,33 +9,13 @@ import TextField from '@mui/material/TextField';
 
 import { DatePickerTheme } from '../../styles/themes/DatePickerTheme';
 
-import { useState, useEffect } from 'react';
-
 const CustomPickersDay = styled(PickersDay, { shouldForwardProp: (prop) => prop !== 'hasVisit' })(({ hasVisit }) => ({
   ...(hasVisit && {
     border: '1px solid #16bac6',
   }),
 }));
 
-export const DatePicker = ({ visits }) => {
-  const [date, setDate] = useState(new Date());
-  const [selected, setSelected] = useState('');
-
-  useEffect(() => {
-    visits.filter((visit) => (visit.data = new Date(visit.date.seconds * 1000 + visit.date.nanoseconds / 1000000)));
-    console.log('visits', visits);
-    const info = visits.find((e) => {
-      console.log('tablica', e);
-      console.log('value', date);
-      console.log('is the same', isSameDay(date, e));
-      isSameDay(date, e);
-    });
-
-    console.log('info', info);
-
-    setSelected(info);
-  }, [date]);
-
+export const DatePicker = ({ visits, date, selected, onChange }) => {
   const renderDay = (date, selectedDates, pickersDayProps) => {
     if (!date) {
       return <PickersDay {...pickersDayProps} />;
@@ -49,12 +29,12 @@ export const DatePicker = ({ visits }) => {
   return (
     <ThemeProvider theme={DatePickerTheme}>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <h3>{date.toString()}</h3>
         <CalendarPicker
+          selected={selected}
           visits={visits}
           date={date}
           minDate={new Date('January 1, 2021 00:00:00')}
-          onChange={(newDate) => setDate(newDate)}
+          onChange={onChange}
           wrapperClassName="date-picker"
           sx={{ backgroundColor: '#fdc161' }}
           renderDay={renderDay}
@@ -63,7 +43,6 @@ export const DatePicker = ({ visits }) => {
             return <TextField {...params} />;
           }}
         />
-        <h4>{selected && selected.toString()}</h4>
       </LocalizationProvider>
     </ThemeProvider>
   );
