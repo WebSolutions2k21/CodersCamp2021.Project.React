aa4ba1189320a4b4b44d16aa5da75ca23858e128
Anna Koruc <aniakoruc@gmail.com>
2022-02-08 21:05:12 +0100

    fix

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -16,16 +16,18 @@ export const AppProvider = ({ children }) => {
   const user = auth.currentUser;
 
   useLayoutEffect(() => {
-    db.collection('users')
-      .where('uid', '==', user.uid)
-      .get()
-      .then(function (q) {
-        q.forEach(function (doc) {
-          setIsAdmin(() => doc.data().isAdmin);
-          setUserName(() => doc.data().firstName);
+    if (user) {
+      db.collection('users')
+        .where('uid', '==', user.uid)
+        .get()
+        .then(function (q) {
+          q.forEach(function (doc) {
+            setIsAdmin(() => doc.data().isAdmin);
+            setUserName(() => doc.data().firstName);
+          });
         });
-      });
-  }, [user.uid]);
+    }
+  }, [user]);
 
   return <AppContext.Provider value={{ userName, isAdmin, iconColor, setIconColor }}>{children}</AppContext.Provider>;
 };
