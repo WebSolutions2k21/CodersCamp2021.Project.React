28ce17bb0494876bddf183ce6779b3cfda840c83
Anna Koruc <aniakoruc@gmail.com>
2022-02-09 22:13:57 +0100

    refactor

--- src/components/UserProfileCard/UserProfileCardStyle.js
+++ src/components/UserProfileCard/UserProfileCardStyle.js
@@ -6,8 +6,8 @@ export const useStyles = makeStyles({
     fontSize: '20px',
   },
   box: {
-    borderRadius: 3,
-    boxShadow: '2px 2px 10px #33333333',
+    borderRadius: '8px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
     padding: '15px',
     background: 'white',
     width: '60%',
