b91ce16375516a111489dae54affd0224dd709a5
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-02-10 12:28:35 +0100

    bugfix: home-page, modal, test (#91)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,44 +6,48 @@ import { Layout, CustomButton } from '../components';
 export const HomePage = () => {
   return (
     <Layout>
-      <Grid container direction="column" justifyContent="center" alignItems="center" style={{ height: '80vh' }}>
-        <Typography
-          component="span"
-          sx={{
-            mb: {
+      <Grid container direction="column" justifyContent="center" alignItems="center">
+        <Grid item sx={{ mt: '15vh' }}>
+          <Typography
+            component="div"
+            sx={{
+              mb: {
+                md: '60px',
+                xs: '30px',
+              },
+              display: {
+                md: 'none',
+                sm: 'block',
+                xs: 'block',
+              },
+            }}
+          >
+            <img src={imgLogo} alt="logo" width="200px"/>
+          </Typography>
+        </Grid>
+        <Grid item sx={{ mt: { md: '10vh' } }}>
+          <Typography
+            variant="h1"
+            fontSize={{
+              lg: '60px',
               md: '60px',
-              xs: '30px',
-            },
-            display: {
-              md: 'none',
-              sm: 'block',
-              xs: 'block',
-            },
-          }}
-        >
-          <img src={imgLogo} alt="logo" />
-        </Typography>
-
-        <Typography
-          variant="h1"
-          fontSize={{
-            lg: '60px',
-            md: '60px',
-            sm: '40px',
-            xs: '35px',
-          }}
-          fontFamily="Ubuntu, sans-serif"
-          paddingBottom="40px"
-          color="#16BAC6"
-          fontWeight="400"
-          letterSpacing="1px"
-        >
-          We love Pets!
-        </Typography>
-
-        <Link to="/Login" style={{ textDecoration: 'none' }}>
-          <CustomButton color="primary" size="large" text="GET STARTED" />
-        </Link>
+              sm: '40px',
+              xs: '35px',
+            }}
+            fontFamily="Ubuntu, sans-serif"
+            paddingBottom="40px"
+            color="#16BAC6"
+            fontWeight="400"
+            letterSpacing="1px"
+          >
+            We love Pets!
+          </Typography>
+        </Grid>
+        <Grid item sx={{pb:"50px"}}>
+          <Link to="/Login" style={{ textDecoration: 'none' }}>
+            <CustomButton color="primary" size="large" text="GET STARTED" />
+          </Link>
+        </Grid>
       </Grid>
     </Layout>
   );
